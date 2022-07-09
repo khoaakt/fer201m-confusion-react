8f9cdb5568549423c36b261235038844645650f2
Nguyễn Anh Khoa <khoaakt@gmail.com>
2022-07-09 11:56:26 +0700

    Controlled Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -52,7 +52,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
-            <Route exact path='/contactus' component={Contact} />} />
+            <Route exact path='/contactus' component={Contact} />
             <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
             <Redirect to="/home" />
         </Switch>
